9b3e84021071c2581ba883c19048ab37f3e0a73d
Nfedans <nfedans@gmail.com>
2021-11-14 14:13:45 +0000

    first upload to repo

--- src/com/company/decryptionFile.java
+++ src/com/company/decryptionFile.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class decrptionFile
+public class decryptionFile
 {
     public static void main(String[] args) throws IOException
     {
@@ -20,17 +20,19 @@ public class decrptionFile
             text2 = input.nextLine().toLowerCase();
 
         }
-        System.out.println(text );
-        System.out.println(text2);
+        System.out.println("Ciphertext 1 : " + text );
+        System.out.println("Ciphertext 2 : " + text2);
 
-        char[] decrpytion = text.toCharArray();
+        char[] decryption = text.toCharArray();
         System.out.println();
-        System.out.println(decrpytion);
+        System.out.println("Cipher text 1 : ");
+        System.out.print(decryption);
 
 
-        for(char cipher: decrpytion)
+        System.out.println("\n\nPlain text 1 : ");
+        for(char cipher: decryption)
         {
-            if(cipher =='a' || cipher=='b' || cipher =='c')
+            if(cipher =='a' || cipher == 'b' || cipher =='c')
             {
                 cipher +=23;
 
@@ -42,13 +44,14 @@ public class decrptionFile
             System.out.print(cipher);
 
         }
-        char[] decrpytion2 = text2.toCharArray();
+        char[] decryption2 = text2.toCharArray();
         System.out.println();
         System.out.println();
-        System.out.println(decrpytion2);
+        System.out.println("\nCipher text 2 : ");
+        System.out.println(decryption2);
 
-
-        for(char cipher2: decrpytion2)
+        System.out.println("\nPlain text 2 : ");
+        for(char cipher2: decryption2)
         {
             if(cipher2 =='x' || cipher2=='y' || cipher2 =='z' ||cipher2 =='w' || cipher2=='s' || cipher2 =='t' || cipher2=='v' || cipher2=='u')
             {
